3acf15822fc79711f122f6415ce8cfc101c6e2b1
mizanur1326 <mizanur1326@gmail.com>
2025-07-23 19:26:48 +0600

    add category and brands default value if submit null in product listing

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -38,7 +38,9 @@ class Product extends Model
 
     public function brand()
     {
-        return $this->belongsTo(Brand::class);
+        return $this->belongsTo(Brand::class)->withDefault([
+            'name' => 'No Brand',
+        ]);
     }
 
 }
