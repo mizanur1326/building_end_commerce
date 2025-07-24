0223022100f94b9cdccc8abd07be7e8da10b1ad2
mizanur1326 <mizanur1326@gmail.com>
2025-07-24 15:06:17 +0600

    add prefix on routes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 // namespace App\Http\Controllers;
 
+
 use App\Http\Controllers\BrandController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\DashboardController;
@@ -17,28 +18,24 @@ use Illuminate\Support\Facades\Route;
 //     return view('admin.pages.dashboard');
 // })->name('dashboard');
 
-Route::get('/', [DashboardController::class, 'index'])->name('dashboard'); 
 
-Route::resource('categories', CategoryController::class);
-
-Route::resource('products', ProductController::class);
-
-Route::delete('/product-images/{image}', [ProductImageController::class, 'destroy'])->name('product-images.destroy');
-
-
-Route::resource('brands', BrandController::class);
+// Route::get('/', [DashboardController::class, 'index'])->name('dashboard'); 
+// Route::resource('categories', CategoryController::class);
+// Route::resource('products', ProductController::class);
+// Route::delete('/product-images/{image}', [ProductImageController::class, 'destroy'])->name('product-images.destroy');
+// Route::resource('brands', BrandController::class);
 
 
 
+Route::prefix('admin')->group(function () {
+    Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
 
-// Route::get('/showCategories', [CategoryController::class, 'index'])->name('showCategories'); 
-// Route::get('/addCategories', [CategoryController::class, 'add'])->name('addCategories'); 
-// Route::get('/addCategories/{id}/edit', [CategoryController::class, 'edit'])->name('categories.edit');
-// Route::put('/addCategories/{id}', [CategoryController::class, 'update'])->name('categories.update');
-// Route::delete('/addCategories/{id}', [CategoryController::class, 'destroy'])->name('categories.destroy');
-// Route::post('/category/store', [CategoryController::class, 'store'])->name('category.store');
+    Route::resource('categories', CategoryController::class);
 
-// Route::get('/addcategories', function () {
-//     return view('admin.pages.categories.addcategories');
-// });
+    Route::resource('products', ProductController::class);
+
+    Route::delete('/product-images/{image}', [ProductImageController::class, 'destroy'])->name('product-images.destroy');
+
+    Route::resource('brands', BrandController::class);
+});
 
